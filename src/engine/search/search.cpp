48b6a3b392e5b0880dcc49ceca57a0bc1b709963
Yash Rallabandi <carbon674@gmail.com>
2025-08-04 12:59:27 +0530

    i am stupid -e bench:

--- src/engine/search/search.cpp
+++ src/engine/search/search.cpp
@@ -261,8 +261,10 @@ namespace episteme::search {
                         for (size_t j = 0; j < explored_quiets.count; j++) {
                             if (explored_quiets.list[j].data() == move.data()) continue;
 
+                            Piece prev_piece = position.mailbox(explored_quiets.list[j].to_square());
+
                             history.update_quiet_hist(position.STM(), explored_quiets.list[j], -bonus);
-                            history.update_cont_hist(stack, piece, explored_quiets.list[j], -bonus, ply);
+                            history.update_cont_hist(stack, prev_piece, explored_quiets.list[j], -bonus, ply);
                         }
                     }
 
