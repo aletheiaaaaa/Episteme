dce67c208cbae856a890eebf44e0b22a66c107cd
aletheiaaaaa <yasaswi.rallabandi@gmail.com>
2025-09-30 19:08:02 +0100

    reduce threshold
    bench: 1753099

--- src/engine/search/search.cpp
+++ src/engine/search/search.cpp
@@ -270,7 +270,7 @@ namespace episteme::search {
                 reduction -= tt_PV;
                 reduction += cut_node * 2;
                 reduction -= history.get_hist(stack, from_pc, to_pc, move, position.STM(), ply, position) / 8192;
-                reduction -= (std::abs(correction) > 150);
+                reduction -= (std::abs(correction) > 80);
 
                 int16_t reduced = std::min(std::max(new_depth - reduction, 1), static_cast<int>(new_depth));
 
