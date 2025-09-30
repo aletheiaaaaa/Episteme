fee4c5101b3e4c747fb0966b60aa868946b1f58b
aletheiaaaaa <yasaswi.rallabandi@gmail.com>
2025-09-30 09:50:54 +0100

    delete cmakefiles
    bench: 1546240

--- CMakeFiles/both.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/both"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/both.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
