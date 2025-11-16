e8e58ab02c1ac57dab69a5052c4752a4b787c824
aletheiaaaaa <yasaswi.rallabandi@gmail.com>
2025-11-16 02:13:47 +0000

    move bench file
    bench: 1575834
    
    bench: 1743757

--- src/engine/search/search.cpp
+++ src/engine/search/search.cpp
@@ -1,5 +1,5 @@
 #include "search.h"
-#include "bench.h"
+#include "../../utils/bench.h"
 
 #include <cassert>
 
@@ -535,9 +535,13 @@ namespace episteme::search {
         int32_t time = params.time[color_idx(position.STM())];
         int32_t inc  = params.inc[color_idx(position.STM())];
 
+        int32_t bound = time / 20 + inc / 2;
+
         SearchLimits limits;
         if (target_nodes) limits.max_nodes = target_nodes;
-        if (time)limits.end = steady_clock::now() + milliseconds(time / 20 + inc / 2);
+        if (time) limits.end = steady_clock::now() + milliseconds(bound);
+
+        time_point<steady_clock> soft_end = steady_clock::now() + milliseconds(bound * 3 / 5);
 
         reset_nodes();
 
@@ -568,7 +572,9 @@ namespace episteme::search {
             for (size_t i = 0; i < report.line.length; ++i) {
                 std::cout << std::format("{} ", report.line.moves[i].to_string());
             }
-            std::cout << "\n";
+            std::cout << std::endl;
+
+            if (limits.time_exceeded() || steady_clock::now() >= soft_end) break;
         }
     
         Move best = last_report.line.moves[0];
