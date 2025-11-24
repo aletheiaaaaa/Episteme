9d6f6459d64986a009fd87d3060cf0e57c32c1e7
aletheiaaaaa <carbon674@gmail.com>
2025-11-24 22:55:02 +0000

    almost!
    bench: 1843828

--- src/engine/search/search.cpp
+++ src/engine/search/search.cpp
@@ -152,8 +152,8 @@ namespace episteme::search {
         }
 
         if (!stack[ply].excluded && !is_PV && !in_check(position, position.STM()) && stack[ply - 1].eval != -INF) {
-            if (depth > 1 && stack[ply - 1].reduction > 3 && static_eval < -stack[ply - 1].eval - 20) depth++;
-            if (depth > 3 && stack[ply - 1].reduction > 1 && static_eval > -stack[ply - 1].eval + 50) depth--;
+            if (depth > 1 && stack[ply - 1].reduction > 3 && static_eval < -stack[ply - 1].eval) depth++;
+            else if (depth > 3 && stack[ply - 1].reduction > 1 && static_eval > -stack[ply - 1].eval) depth--;
         }
 
         if (!stack[ply].excluded && !in_check(position, position.STM())) {
