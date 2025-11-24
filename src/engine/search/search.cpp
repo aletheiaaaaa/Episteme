cdb94378b017bf361841cee267fec9cf041bb564
aletheiaaaaa <carbon674@gmail.com>
2025-11-24 23:42:12 +0000

    final
    bench: 1721204
    
    bench: 1702266

--- src/engine/search/search.cpp
+++ src/engine/search/search.cpp
@@ -146,8 +146,7 @@ namespace episteme::search {
         }
 
         if (!stack[ply].excluded && !is_PV && !in_check(position, position.STM()) && stack[ply - 1].eval != -INF) {
-            if (depth > 1 && stack[ply - 1].reduction > 3 && static_eval < -stack[ply - 1].eval) depth++;
-            else if (depth > 3 && stack[ply - 1].reduction > 1 && static_eval > -stack[ply - 1].eval) depth--;
+            if (depth > 1 && stack[ply - 1].reduction > 3 && static_eval + stack[ply - 1].eval < 0) depth++;
         }
 
         if (!stack[ply].excluded && !in_check(position, position.STM())) {
