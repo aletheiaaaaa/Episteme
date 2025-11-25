32b7399facb4d5362b09bad8e3970f9de150797a
aletheiaaaaa <carbon674@gmail.com>
2025-11-25 00:52:13 +0000

    tigten hindsight reductions
    bench: 1440586

--- src/engine/search/search.cpp
+++ src/engine/search/search.cpp
@@ -153,7 +153,7 @@ namespace episteme::search {
 
         if (!stack[ply].excluded && !is_PV && !in_check(position, position.STM()) && stack[ply - 1].eval != -INF) {
             if (depth > 1 && stack[ply - 1].reduction > 3 && static_eval + stack[ply - 1].eval < -20) depth++;
-            else if (depth > 3 && stack[ply - 1].reduction > 1 && static_eval + stack[ply - 1].eval > 50) depth--;
+            else if (depth > 3 && stack[ply - 1].reduction > 1 && static_eval + stack[ply - 1].eval > 30) depth--;
         }
 
         if (!stack[ply].excluded && !in_check(position, position.STM())) {
