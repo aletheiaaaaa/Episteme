8413d86e68def23302f32608f4e0cc1cb80e5a15
aletheiaaaaa <carbon674@gmail.com>
2025-11-25 22:27:49 +0000

    i am desparate
    bench: 1725901

--- src/engine/search/search.cpp
+++ src/engine/search/search.cpp
@@ -151,9 +151,9 @@ namespace episteme::search {
             }
         }
 
-        if (!stack[ply].excluded && !is_PV && !in_check(position, position.STM())) {
-            if (depth > 1 && stack[ply - 1].eval != -INF && stack[ply - 1].reduction > 3 && static_eval + stack[ply - 1].eval < -20) depth++;
-        }
+        // if (!stack[ply].excluded && !is_PV && !in_check(position, position.STM())) {
+        //     if (depth > 1 && stack[ply - 1].eval != -INF && stack[ply - 1].reduction > 3 && static_eval + stack[ply - 1].eval < -20) depth++;
+        // }
 
         if (!stack[ply].excluded && !in_check(position, position.STM())) {
             if (!is_PV && depth <= 5 && static_eval >= beta + std::max(depth - improving, 0) * 100 + std::abs(correction) / 4) return static_eval;
