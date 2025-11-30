124976f6d3d6e27d8081c8efbe88008baffd03d9
aletheiaaaaa <carbon674@gmail.com>
2025-11-30 04:53:23 +0000

    prevent see threshold reductions
    bench: 1350886

--- src/engine/search/search.cpp
+++ src/engine/search/search.cpp
@@ -217,7 +217,7 @@ namespace episteme::search {
 
                 const int32_t see_threshold = (is_quiet) ? 
                     quiet_see_base() + quiet_see_mult() * depth : 
-                    noisy_see_base() + noisy_see_mult() * depth * depth - std::clamp(history.get_capt_hist(from_pc, move, to_pc), -noisy_see_hist_max(), noisy_see_hist_max()) / 32;
+                    noisy_see_base() + noisy_see_mult() * depth * depth - std::clamp(history.get_capt_hist(from_pc, move, to_pc), -noisy_see_hist_max(), 0) / 32;
                 if (!is_PV && !eval::SEE(position, move, see_threshold)) continue;
 
                 const int32_t history_margin = (is_quiet) ? hist_prune_quiet_base() + hist_prune_quiet_mult() * depth : hist_prune_noisy_base() + hist_prune_noisy_mult() * depth;
