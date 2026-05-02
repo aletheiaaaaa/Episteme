6b8f129bc41d2e7f03dac43e4c32186cb5af4363
aletheiaaaaa <carbon674@gmail.com>
2026-05-02 16:48:30 +0100

    include tt refactor
    bench: 1690001

--- src/engine/search/ttable.hpp
+++ src/engine/search/ttable.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#pragma once
 
 #include <cstdint>
 #include <vector>
@@ -17,20 +18,38 @@ struct Entry {
   bool tt_PV = false;
 };
 
+struct Packed {
+  uint64_t hash = 0;
+  int32_t score = 0;
+  uint16_t move_data = 0;
+  uint8_t depth = 0;
+  uint8_t misc = 0;
+
+  Packed() = default;
+  Packed(const Entry& entry)
+    : hash(entry.hash),
+      move_data(entry.move.data()),
+      score(entry.score),
+      depth(entry.depth),
+      misc(static_cast<uint8_t>(entry.node_type) | (static_cast<uint8_t>(entry.tt_PV) << 2)) {}
+};
+
 class Table {
   public:
   Table(uint32_t size) {
-    const size_t entries = (size * 1024 * 1024) / sizeof(Entry);
-    ttable.resize(entries);
+    const size_t num_entries = (size * 1024 * 1024) / sizeof(Packed);
+    ttable.resize(num_entries);
   }
 
   void resize(uint32_t size) {
     ttable.clear();
-    const size_t entries = (size * 1024 * 1024) / sizeof(Entry);
-    ttable.resize(entries);
+    const size_t num_entries = (size * 1024 * 1024) / sizeof(Packed);
+    ttable.resize(num_entries);
   }
 
-  void reset() { std::fill(ttable.begin(), ttable.end(), Entry()); }
+  void reset() {
+    std::fill(ttable.begin(), ttable.end(), Packed{});
+  }
 
   uint64_t table_index(uint64_t hash) {
     return static_cast<uint64_t>(
@@ -42,14 +61,14 @@ class Table {
     uint64_t index = table_index(hash);
     Entry entry;
 
-    if (ttable[index].hash == hash) entry = ttable[index];
+    if (ttable[index].hash == hash) entry = get_entry(index);
 
     return entry;
   }
 
   void add(Entry tt_entry) {
     uint64_t index = table_index(tt_entry.hash);
-    ttable[index] = tt_entry;
+    ttable[index] = Packed(tt_entry);
   }
 
   int32_t hashfull() const {
@@ -63,7 +82,18 @@ class Table {
     return (filled * 1000) / sample_size;
   }
 
+  Entry get_entry(int idx) const {
+    return Entry{
+      .hash = ttable[idx].hash,
+      .score = ttable[idx].score,
+      .move = Move(ttable[idx].move_data),
+      .depth = ttable[idx].depth,
+      .node_type = NodeType(ttable[idx].misc & 0b11),
+      .tt_PV = static_cast<bool>((ttable[idx].misc >> 2) & 0b1)
+    };
+  }
+
   private:
-  std::vector<Entry> ttable;
+  std::vector<Packed> ttable;
 };
 }  // namespace episteme::tt
